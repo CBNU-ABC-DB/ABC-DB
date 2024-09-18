9cbe6754d27600e9016c22f5dbcb6a463f02c635
jjhwan-h <jjhwan.h@gmail.com>
2024-09-18 09:03:29 +0900

    feat: file 구조

--- dockerfile
+++ dockerfile
@@ -12,12 +12,12 @@ RUN apk update && apk add --no-cache \
 
 WORKDIR /app
 
-COPY ./src .
-
 RUN wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz && \
     tar -xzf boost_1_82_0.tar.gz && \
     cd boost_1_82_0 && \
     ./bootstrap.sh && \
     ./b2 install --with-serialization --with-filesystem --prefix=/usr/local
 
-RUN g++ page.cpp -o page -L/usr/local/lib -lboost_serialization -lboost_filesystem -lboost_iostreams -lboost_system
+COPY ./src .
+
+RUN g++ main.cpp file.cpp page.cpp -o main -L/usr/local/lib -lboost_serialization -lboost_filesystem -lboost_iostreams -lboost_system
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,58 @@
 #include "page.h"
+#include "file.h"
+#include <iostream>
+
+
+void search(File& f);
+void iter(File& f, PageDirectory& dir);
 
 int main() {
-    Page page(5);  
+    std::shared_ptr<Page> p;
 
-    const char* record1 = "1231243";
-    const char* record2 = "S123xRecord";
-    page.InsertRecord(record1, std::strlen(record1) + 1); 
-    page.InsertRecord(record2, std::strlen(record2) + 1); 
+    /**
+     * @brief 
+     *  TODO::table생성 시 파일 및 첫번재 페이지 디렉토리 자동 생성.
+     *   지금은 처음 실행 시 
+     *   아래 주석 해제 후 실행
+     *   주석 후 빌드
+     *   다시 실행
+     */
+    File f("student.bin");
+    // PageDirectory directory(0);
+    // f.WritePageDirectoryToFile(directory);
+    
+    std::shared_ptr<PageDirectory> dir = f.GetPageDir(); 
 
-    page.WriteToFile("page_data.bin"); 
+    p = std::make_shared<Page>();
+    f.AddPageToDirectory(*dir,*p);
+  
+    //레코드 삽입 및 직렬화
+    const char* record1 = "jjang";
+    const char* record2 = "1458%%$";
+    p.get()->InsertRecord(record1, std::strlen(record1) + 1);
+    p.get()->InsertRecord(record2, std::strlen(record2) + 1);
+    f.WritePageToFile(*dir,*p);
+    f.WritePageDirectoryToFile(*dir);
 
-    Page loaded_page(5);               
-    loaded_page.ReadFromFile("page_data.bin"); 
-
-    loaded_page.PrintRecord(); 
-
+    //페이지디렉토리가 가리키는 페이지의 레코드 전부출력
+    search(f);
     return 0;
+}
+
+void search(File& f){
+    std::shared_ptr<PageDirectory> dir = f.GetPageDir(); 
+    while(dir->GetNext()){
+        iter(f, *dir);
+        size_t offset = dir->GetNext();
+        dir = f.GetPageDir(offset);
+    }
+    iter(f, *dir);
+}
+void iter(File& f, PageDirectory& dir){
+    const auto& entries = dir.GetEntries();
+    for(int i=0; i<dir.GetSize(); i++){
+        std::cout<<"page인덱스: "<<i<<std::endl;
+        std::shared_ptr<Page> p2 = f.GetPage(dir,i);
+        p2->PrintRecord();
+    }
 }
