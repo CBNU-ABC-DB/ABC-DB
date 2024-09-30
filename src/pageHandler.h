0769ee2c8184391a9592176ab1a440cc7e076a6a
binwon-song <binwon236@gmail.com>
2024-09-30 11:22:20 +0900

    add buffer manager : select one Page from buffer pool

--- src/pageHandler.h
+++ src/pageHandler.h
@@ -12,12 +12,11 @@ private:
     Page *firstPage; //자주 접근 페이지
     Page *midPage; // 중간 페이지 : page 삽입 지점 last 방향으로 삽입
     Page *lastPage; //잘 접근 되지않는 페이지
-
     /**
      * @brief 버퍼 풀을 만드는 함수 first, mid, last page를 설정
      * @param page first page
      */
-    Page *InitBufferPool(Page *page);
+    Page *InitTempBufferPool(Page *page);
     int pCurrnetSize;  // 현재 사용 중인 페이지 수
     int pTotalSize;   // 총 페이지 수
     int midPoint;
@@ -25,12 +24,12 @@ public:
     PageHandler()
         : firstPage(new Page()), pTotalSize(PAGE_AMOUNT), pCurrnetSize(1),midPoint(PAGE_AMOUNT*5/8)
         {
-            InitBufferPool(firstPage);
+            InitTempBufferPool(firstPage);
         }
     /**
      * @brief 버퍼 풀로 부터 페이지를 가져옴
      */
-    Page* GetBlockFromBufferPool();
+    Page* FillBufferPoolPage();
 
     /**
      * @brief 페이지를 버퍼 풀에 반환
@@ -44,6 +43,9 @@ public:
     Page* getMidPage() { return midPage; }
 
     Page* getLastPage() { return lastPage; }
+
+    int GetpCurrnetSize() { return pCurrnetSize; }
+    int GetpTotalSize() { return pTotalSize; }
 
 };
 
