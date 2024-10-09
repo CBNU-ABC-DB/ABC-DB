de0997363da018c1b6f3734a9b146c1a53232368
nam3762 <nam37623762@gmail.com>
2024-10-09 18:08:26 +0900

    CREATE DB, DROP DB, CREATE TABLE, DROP TABLE, DELECT TABLE, INSERT DATA, DELECT DATA

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate lexer and parser from SQL.g4 using ANTLR4
-java -jar /opt/antlr4/antlr-4.13.2-complete.jar -Dlanguage=Cpp -visitor -o SQL_Grammar SQL.g4
+java -jar /opt/antlr4/antlr-4.13.2-complete.jar -Dlanguage=Cpp -visitor -o /app/SQL_Grammar /app/SQL_Grammar/SQL.g4
 
 # Compile the project with the generated lexer and parser files using C++17
 g++ -std=c++17 \
@@ -9,10 +9,10 @@ g++ -std=c++17 \
     -I./SQL_Grammar \
     -I. \
     main.cpp \
+    SQL_Grammar/*.cpp \
     catalog_manager.cpp \
     SQLASTBuilder.cpp \
     SQLSemanticAnalyzer.cpp \
-    SQL_Grammar/*.cpp \
     -lantlr4-runtime \
     -lboost_serialization \
     -lboost_system \
